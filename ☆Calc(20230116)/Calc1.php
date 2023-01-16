946681be849aaefd39a857d9f359654b84b6ce23
lighttobrca <quwieorpt@gmail.com>
2023-01-16 18:27:33 +0900

    20230116 - 18:27 calcProgram Commit
    20230116 - 18:27 calcProgram Commit

--- ☆Calc(20230116)/Calc1.php
+++ ☆Calc(20230116)/Calc1.php
@@ -7,26 +7,53 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="calcResult.php" method="POST">
-        <input type="textfield" id="Result1" value="" width="100" height="100">
+    <form name="dentaku">
         <table>
+            <input type="text" class="display" name="display" value="" disabled>
         <tr>
-        <td><input type="button" id="b1" value="1"></td><td><input type="button" id="b3" value="3"></td><td><input type="button" id="b3" value="3"></td>
+        <td><input type="button" id="b1" value="1" onclick="get_calc(this)"></td>
+        <td><input type="button" id="b2" value="2" onclick="get_calc(this)"></td>
+        <td><input type="button" id="b3" value="3" onclick="get_calc(this)"></td>
         </tr>
         <tr>
-        <td><input type="button" id="b4" value="4"></td><td><input type="button" id="b5" value="5"></td><td><input type="button" id="b6" value="6"></td>
+        <td><input type="button" id="b4" value="4" onclick="get_calc(this)"></td>
+        <td><input type="button" id="b5" value="5" onclick="get_calc(this)"></td>
+        <td><input type="button" id="b6" value="6" onclick="get_calc(this)"></td>
         </tr>
         <tr>
-        <td><input type="button" id="b7" value="7"></td><td><input type="button" id="b8" value="8"></td><td><input type="button" id="b9" value="9"></td>
+        <td><input type="button" id="b7" value="7" onclick="get_calc(this)"></td>
+        <td><input type="button" id="b8" value="8" onclick="get_calc(this)"></td>
+        <td><input type="button" id="b9" value="9" onclick="get_calc(this)"></td>
         </tr>
         <tr>
-        <td><input type="button" id="b0" value="0"></td>
+        <td><input type="button" id="b0" value="0" onclick="get_calc(this)"></td>
+        <td><input type="button" id="bC" value="C" onclick="get_calc(this)"></td>
+        <td><input type="button" id="bE" value="=" onclick="get_calc(this)"></td>
         </tr>
         <tr>
-            <td><input type="button" id="+" value="+"></td><td><input type="button" id="-" value="-"></td><td><input type="button" id="*" value="*"></td><td><input type="button" id="/" value="/"></td>
+            <td><input type="button" id="+" value="+" onclick="get_calc(this)"></td>
+            <td><input type="button" id="-" value="-" onclick="get_calc(this)"></td>
+            <td><input type="button" id="*" value="×" name="multi_btn" onclick="get_calc(this)"></td>
+            <td><input type="button" id="/" value="/" name="div_btn" onclick="get_calc(this)"></td>
         </tr>
-    <?php
-    
-    ?>
+
+<script>
+ function get_calc(btn){
+    if(btn.value == "="){
+        document.dentaku.display.value = eval(document.dentaku.display.value);
+    }else if(btn.value == "C"){
+        document.dentaku.display.value = "";
+    }else{
+        if(btn.value == "×"){
+            bnt.value = "*";
+        }else if(btn.value == "/"){
+            btn.value = "/";
+    }
+    document.dentaku.display.value += btn.value;
+    document.dentaku.multi_btn.value="×";
+    document.dentaku.div_btn.value="/";
+    }
+}
+</script>
 </body>
 </html>
