37762694a18e206a510ed96cafd2b4a109f8d9ed
lighttobrca <quwieorpt@gmail.com>
2023-01-16 15:52:11 +0900

    20230116 camera&calc commit
    20230116 camera&calc commit

---
+++ ☆Calc(20230116)/Calc1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="calcResult.php" method="POST">
+        <input type="textfield" id="Result1" value="" width="100" height="100">
+        <table>
+        <tr>
+        <td><input type="button" id="b1" value="1"></td><td><input type="button" id="b3" value="3"></td><td><input type="button" id="b3" value="3"></td>
+        </tr>
+        <tr>
+        <td><input type="button" id="b4" value="4"></td><td><input type="button" id="b5" value="5"></td><td><input type="button" id="b6" value="6"></td>
+        </tr>
+        <tr>
+        <td><input type="button" id="b7" value="7"></td><td><input type="button" id="b8" value="8"></td><td><input type="button" id="b9" value="9"></td>
+        </tr>
+        <tr>
+        <td><input type="button" id="b0" value="0"></td>
+        </tr>
+        <tr>
+            <td><input type="button" id="+" value="+"></td><td><input type="button" id="-" value="-"></td><td><input type="button" id="*" value="*"></td><td><input type="button" id="/" value="/"></td>
+        </tr>
+    <?php
+    
+    ?>
+</body>
+</html>
